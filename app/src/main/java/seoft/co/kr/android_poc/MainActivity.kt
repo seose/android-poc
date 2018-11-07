3d4be7792f3664960abb395fe0a7bb5b186c1bf6
seoft <32695143+seose@users.noreply.github.com>
2018-11-07 20:32:30 +0900

    Init new activity

---
+++ app/src/main/java/seoft/co/kr/android_poc/MainActivity.kt
@@ -0,0 +1,12 @@
+package seoft.co.kr.android_poc
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
