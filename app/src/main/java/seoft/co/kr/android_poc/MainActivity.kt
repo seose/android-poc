9211c4ba4075ea6db381218b2f5ec6c6cc41c30e
seoft <32695143+seose@users.noreply.github.com>
2018-11-26 22:24:53 +0900

    done

--- app/src/main/java/seoft/co/kr/android_poc/MainActivity.kt
+++ app/src/main/java/seoft/co/kr/android_poc/MainActivity.kt
@@ -1,11 +1,20 @@
 package seoft.co.kr.android_poc
 
-import android.support.v7.app.AppCompatActivity
+import android.graphics.Bitmap
+import android.graphics.Color
 import android.os.Bundle
-import android.view.View
+import android.support.v7.app.AppCompatActivity
 import kotlinx.android.synthetic.main.activity_main.*
+import me.priyesh.chroma.ChromaDialog
+import me.priyesh.chroma.ColorMode
+import me.priyesh.chroma.ColorSelectListener
+import seoft.co.kr.android_poc.util.i
 
+
+// ref : https://github.com/ItsPriyesh/chroma
 class MainActivity : AppCompatActivity(){
+
+    val COLOR_DECODER = "\"#%06X\""
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -30,9 +39,30 @@ class MainActivity : AppCompatActivity(){
 
     fun AAA() {
 
+        //        ChromaUtil.getFormattedColorString(color,false).toString().i("222")
+        ChromaDialog.Builder()
+                .initialColor(Color.GREEN)
+                .colorMode(ColorMode.RGB) // There's also ARGB and HSV
+                .onColorSelected(object : ColorSelectListener{
+                    override fun onColorSelected(color: Int) {
+                        val stringHexCode = String.format(COLOR_DECODER, 0xFFFFFF and color)
+
+                        stringHexCode.i("stringHexCode")
+
+                        val bit = Bitmap.createBitmap(1,1,Bitmap.Config.ARGB_8888)
+                        bit.eraseColor(color)
+
+                        iv.setImageBitmap(bit)
+                    }
+
+                })
+                .create()
+                .show(getSupportFragmentManager(), "ChromaDialog");
     }
 
+
     fun BBB() {
+
 
     }
 
