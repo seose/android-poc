08fb1af8e52808542d7aa67a49d0428d1583cba8
seoft <32695143+seose@users.noreply.github.com>
2019-03-16 19:06:19 +0900

    done

---
+++ app/src/main/java/seoft/co/kr/android_poc/BlackScreen.kt
@@ -0,0 +1,28 @@
+package seoft.co.kr.android_poc
+
+import android.os.Bundle
+import android.os.Handler
+import android.support.v7.app.AppCompatActivity
+import android.view.View
+import android.view.WindowManager
+
+class BlackScreen : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        window.setFlags(WindowManager.LayoutParams.FLAG_LAYOUT_NO_LIMITS,
+                WindowManager.LayoutParams.FLAG_LAYOUT_NO_LIMITS)
+
+        setContentView(R.layout.activity_black_screen)
+
+        window.decorView.systemUiVisibility = View.SYSTEM_UI_FLAG_FULLSCREEN
+        window.decorView.systemUiVisibility = View.SYSTEM_UI_FLAG_HIDE_NAVIGATION
+                actionBar?.hide()
+
+        Handler().postDelayed( {
+            finish()
+        },3000)
+
+
+    }
+}
--- app/src/main/java/seoft/co/kr/android_poc/MainActivity.kt
+++ app/src/main/java/seoft/co/kr/android_poc/MainActivity.kt
@@ -1,15 +1,25 @@
 package seoft.co.kr.android_poc
 
-import android.support.v7.app.AppCompatActivity
+import android.content.Intent
 import android.os.Bundle
-import android.view.View
+import android.os.Handler
+import android.provider.Settings
+import android.support.v7.app.AlertDialog
+import android.support.v7.app.AppCompatActivity
+import android.util.Log
 import kotlinx.android.synthetic.main.activity_main.*
+
 
 class MainActivity : AppCompatActivity(){
 
+    val TAG = "MainActivity#$#"
+
+    val REQ_PERMISSIONS = 101
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        reqPermission()
 
 		btA.setOnClickListener { v ->
             AAA()
@@ -28,7 +38,43 @@ class MainActivity : AppCompatActivity(){
         }
     }
 
+    fun reqPermission(){
+        val settingsCanWrite = Settings.System.canWrite(this)
+        if (!settingsCanWrite) {
+            val intent = Intent(Settings.ACTION_MANAGE_WRITE_SETTINGS)
+            startActivity(intent)
+        } else {
+            val alertDialog = AlertDialog.Builder(this@MainActivity).create()
+            alertDialog.setMessage("You have system write settings permission now.")
+            alertDialog.show()
+        }
+    }
+
+
     fun AAA() {
+
+        val defaultTurnOffTime = Settings.System.getInt(contentResolver, Settings.System.SCREEN_OFF_TIMEOUT, 30000)
+        val defaultBrightness = Settings.System.getInt(contentResolver, Settings.System.SCREEN_BRIGHTNESS, 150)
+
+        Log.i(TAG,defaultTurnOffTime.toString())
+
+        Settings.System.putInt(contentResolver,Settings.System.SCREEN_OFF_TIMEOUT, 1)
+        Settings.System.putInt(contentResolver,Settings.System.SCREEN_BRIGHTNESS, 0)
+        Log.i(TAG,"Settings.System.putInt(contentResolver,Settings.System.SCREEN_OFF_TIMEOUT, 1000)")
+
+        val intent = Intent(applicationContext,BlackScreen::class.java).apply {
+            addFlags(Intent.FLAG_ACTIVITY_NO_HISTORY)
+            addFlags(Intent.FLAG_ACTIVITY_NO_ANIMATION)
+        }
+
+        startActivity(intent)
+
+        Handler().postDelayed( {
+            Settings.System.putInt(contentResolver,Settings.System.SCREEN_OFF_TIMEOUT, defaultTurnOffTime)
+            Settings.System.putInt(contentResolver,Settings.System.SCREEN_BRIGHTNESS, defaultBrightness)
+            Log.i(TAG,"Settings.System.putInt(contentResolver,Settings.System.SCREEN_OFF_TIMEOUT, defaultTurnOffTime)")
+
+        },3000)
 
     }
 
@@ -38,9 +84,13 @@ class MainActivity : AppCompatActivity(){
 
     fun CCC() {
 
+
     }
 
     fun DDD() {
 
+
     }
+
+
 }
